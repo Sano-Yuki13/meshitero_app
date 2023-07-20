958390be2418c5bc5afe2a6837e352198ff71df5
SanoYuki <134158109+Sano-Yuki13@users.noreply.github.com>
2023-07-20 12:04:46 +0000

    finish 19

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'post_images/new'
+  get 'post_images/index'
+  get 'post_images/show'
+  devise_for :users
   root to: "homes#top"
+  get '/about', to: 'homes#about'
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :favorites, only: [:create, :destroy]
+   resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
